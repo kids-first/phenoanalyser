c868a85d1fd8b13711acd6038450a26bcc6f7b81
Michele Mattioni <michele.mattioni@velsera.com>
2023-08-11 14:17:07 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Phenoanalyser
+
+The main goal of Phenoanalyser is to find pattern and possible connections in the phenotypes of different
+patients, belonging to different datasets, using the variants of interest as the inputs.
+
+Phenoanalyser queries the [Kids First FHIR Server](https://kf-api-fhir-service.kidsfirstdrc.org/) as the source, 
+but it will be extend to others FHIR Server in the future.
+
+
---
+++ main.py
@@ -0,0 +1,27 @@
+"""
+Steps:
+
+1) Retrieve patient information based on variants
+2) Traverse Patient and organize them via HPO terms
+3) Build Clustering of HPO terms
+
+
+
+# Interesting packages:
+
+https://pyhpo.readthedocs.io/en/latest/
+https://pheatmap.readthedocs.io/en/stable/BasicUsage.html
+
+# Interesting papers:
+
+Shu, B. et al. (2022). 
+Human phenotype ontology annotation and cluster analysis for pulmonary atresia to unravel clinical outcomes. 
+Frontiers in Cardiovascular Medicine, 9, 898289. https://doi.org/10.3389/fcvm.2022.898289
+
+Westbury, S. K. et al. BRIDGE-BPD Consortium. (2015). Human phenotype ontology annotation and cluster analysis #
+to unravel genetic defects in 707 cases with unexplained bleeding and platelet disorders. 
+Genome Medicine, 7(1), 36. https://doi.org/10.1186/s13073-015-0151-5
+
+"""
+
+
