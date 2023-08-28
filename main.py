ab0493d1d8ad5783efd9b0c902da3efbe7f0461b
Michele Mattioni <michele.mattioni@velsera.com>
2023-08-28 18:58:39 +0200

    Getting the version with the cookie for reference

--- main.py
+++ main.py
@@ -25,3 +25,46 @@ Genome Medicine, 7(1), 36. https://doi.org/10.1186/s13073-015-0151-5
 """
 
 
+import argparse
+import datetime
+import requests
+import simplejson
+import pprint
+
+
+class FHIRRetriever:
+    def __init__(self, fhir_auth_cookie, kids_first_fhir_url="https://kf-api-fhir-service.kidsfirstdrc.org/"):
+        self.fhir_auth_cookie = fhir_auth_cookie
+        self.KIDS_FIRST_FHIR= kids_first_fhir_url
+
+    def retrieve_hpo_terms(self, patients):
+        """Retrieve the hpo terms for each patients, returning a dictionary with key the Patient Identifier and
+        the value the list of HPO terms found"""
+        #https://kf-api-fhir-service.kidsfirstdrc.org/Condition?patient.identifier=PT_8NNFJYG5&_format=json
+        
+        # patient_id = "PT_8NNFJYG5"
+        for patient_id in patients:
+            target_url = f"{self.KIDS_FIRST_FHIR}/Condition?patient.identifier={patient_id}&format=json"
+            req = requests.get(target_url)
+            try:
+                req_j = req.json()
+                for entry in req_j['entry']:
+                    for entry in entries:
+                        code = entry['resource']['code']
+                        pprint.pprint(code)
+                        print("-"*40 + "\n")
+            except KeyError:
+                print ("Unable to serialize to JSON")
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Retrieve HPO terms from the Kids First FHIR Server')
+    parser.add_argument("--include_fhir_authentication_cookie", required=True, 
+                        help="The Authorization cookie from the INCLUDE FHIR Server (https://kf-api-fhir-service.kidsfirstdrc.org/) \
+                            To obtain the cookie, open the Chorme or Firefox console, go to the Application tab and copy the value \
+                            contained in `AWSELBAuthSessionCookie-0`.")
+    args = parser.parse_args()
+    fhir_retriever = FHIRRetriever(args.include_fhir_authentication_cookie)
+    req = fhir_retriever.retrieve_hpo_terms(["PT_8NNFJYG5"])
+
